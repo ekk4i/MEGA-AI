c720f332c2ec057ba636a25deff5c9ae13a46906
ekk4i <johnisaboyhouse@gmail.com>
2025-01-15 16:41:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '237677088573';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '237677088573' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
